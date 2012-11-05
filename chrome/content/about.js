6f739913dee85931ed95343c3745dccc1f124d24
Ferenc Szekely <ferenc.szekely@gmail.com>
2012-11-05 14:34:54 +0200

    removing legacy code

--- chrome/content/about.js
+++
@@ -1,6 +0,0 @@
-var glome = Components.classes["@glome.me/glome-ext;1"].createInstance().wrappedJSObject;
-
-function fillInVersion() {
-  var versionField = document.getElementById("version");
-  versionField.value = abp.getInstalledVersion();
-}
